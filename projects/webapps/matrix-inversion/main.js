9febce42063acb426948310f686860248a64b989
simonglitch <64504202+simon-glitch@users.noreply.github.com>
2023-09-13 09:56:58 -0400

    added big comment

--- projects/webapps/matrix-inversion/main.js
+++ projects/webapps/matrix-inversion/main.js
@@ -29,8 +29,32 @@ invert.is_upper_triangle = function is_upper_triangle(m){
   }
   return true;
 };
-invert.has_zero_row = function has_zero_row(m){};
-invert.has_zero_col = function has_zero_col(m){};
+invert.has_zero_row = function has_zero_row(m){
+  outer_row: for(let i = 0; i < m.length; i++){
+    for(let j = 0; j < m[i].length; j++){
+      // this condition causes us to continue the outer loop. this means that the code below this inner for loop will get skipped, because continue skips all code in a for loop (which comes after the continue statement).
+      if(m[i][j] !== 0){
+        continue outer_row;
+      }
+    }
+    // due the continue statement above, the inner loop can only finish and get to this line of code if the row is full of zeroes.
+    return true;
+  }
+  return false;
+};
+invert.has_zero_col = function has_zero_col(m){
+  outer_col: for(let i = 0; i < m[0].length; i++){
+    for(let j = 0; j < m.length; j++){
+      // this condition causes us to continue the outer loop. this means that the code below this inner for loop will get skipped, because continue skips all code in a for loop (which comes after the continue statement).
+      if(m[j][i] !== 0){
+        continue outer_col;
+      }
+    }
+    // due the continue statement above, the inner loop can only finish and get to this line of code if the column is full of zeroes.
+    return true;
+  }
+  return false;
+};
 
 invert.det  = function find_det(m){
   if(m.length !== m[0].length){
@@ -121,8 +145,8 @@ invert.print = function print_matrix(m){
       mi.map(
         (mij) => (mij.toFixed(2))
       ).join(", ")
-    )).join("\n  ");
-  str = "[" + str + "\n]"
+    )).join(";\n  ");
+  str = "m = [\n  " + str + "\n]"
   str += ",\n  det = " + (typeof m.det === "number" ? m.det.toFixed(3) : m.det);
   str += ",\n  branches = " + m.branches;
   return str;
@@ -164,4 +188,44 @@ console.log(p( invert.det([
   [0,-2,1, 7],
 ]) ));
 
+let rand = function(h, w){
+  let r = function(){
+    return Math.floor(Math.random() * 11) - 5;
+  };
+  let m = [];
+  for(let i = 0; i < h; i++){
+    m[i] = [];
+    for(let j = 0; j < w; j++){
+      m[i][j] = r();
+    }
+  }
+  return m;
+};
+
+console.log(p( invert.det(rand(5,5)) ));
+console.log(p( invert.det(rand(6,6)) ));
+console.log(p( invert.det(rand(7,7)) ));
+
+/*
+  branches required with this brute force method:
+  2x2:                    1,
+  3x3:                    4,
+  4x4:                   17,
+  5x5:                   86,
+  6x6:                  517,
+  7x7:                3_620,
+  8x8:               28_961,
+  9x9:              260_650,
+  10x10:          2_606_501,
+  11x11:         28_671_512,
+  12x12:        344_058_145,
+  13x13:      4_472_755_886,
+  14x14:     62_618_582_405,
+  15x15:    939_278_736_076,
+  16x16: 15_028_459_777_217,
+  
+  // so, unfortunately, my computer can not calculate the determinant of a 16x16 matrix using these methods.
+  
+  n x n: ~= 11.4893 * n!
+*/
 
