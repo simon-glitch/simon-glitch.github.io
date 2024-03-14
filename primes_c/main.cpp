d63c352142564fb219fefc0cc4e798fe3c42952a
Simon Willover <simontalksalot@gmail.com>
2024-03-14 10:49:25 -0400

    new stuff

---
+++ primes_c/main.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <vector>
+#include <cmath>
+using namespace std;
+
+// don't need primes larger than 4 billion
+typedef unsigned uint;
+typedef long long lint;
+typedef unsigned long long ulint;
+vector<uint> primes;
+
+inline bool my_modr(uint p, uint n){
+    uint ln = log2((double) p);
+    uint *is = new uint[ln];
+    for(uint i = 0; i < ln; i++){
+        is[i];
+    }
+}
+
+int main(){
+    return 0;
+}
