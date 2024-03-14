67083a6ad1c4ffefd5823d63dee5b35614fd07d0
Simon Willover <simontalksalot@gmail.com>
2024-03-14 11:02:20 -0400

    fun stuff

--- primes_c/main.cpp
+++ primes_c/main.cpp
@@ -9,12 +9,26 @@ typedef long long lint;
 typedef unsigned long long ulint;
 vector<uint> primes;
 
-inline bool my_modr(uint p, uint n){
+inline uint my_modr(uint p, uint n){
     uint ln = log2((double) p);
     uint *is = new uint[ln];
-    for(uint i = 0; i < ln; i++){
-        is[i];
+    is[0] = p % n;
+    // both of these for loops can be torn apart into 64-line switch statements
+    for(uint i = 1; i < ln; i++){
+        // these ulint conversions are to ensure that this algorithm works correctly for n >= 65536
+        is[i] = ((ulint) is[i-1] * (ulint) is[i-1]) % (ulint) n;
     }
+    inline uint res = 1;
+    // both of these for loops can be torn apart into 64-line switch statements
+    for(uint i = 1; i < ln; i++){
+        /*
+        Another way to right this:
+          bool do_mul = n & (1 << i);
+          res = (res*((is[i] * do_mul + !do_mul))) % n;
+        */
+        res = (res*((n & (1 << i)) ?is[i] :1)) % n;
+    }
+    return res;
 }
 
 int main(){
