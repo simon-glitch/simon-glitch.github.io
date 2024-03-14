ad880e665600bad93e391f0a7bb17556ec6a7c4d
Simon Willover <simontalksalot@gmail.com>
2024-03-14 15:43:02 -0400

    add macros for these functions

--- primes_c/main.cpp
+++ primes_c/main.cpp
@@ -31,20 +31,46 @@ inline uint my_modr(uint p, uint n){
     return res;
 }
 
-inline uint cube(const uint n){
-    return n * n * n;
+#define f_of_x(itype, otype, name, exp) \
+inline otype name(const itype n){\
+    return (exp);\
 }
-inline ulint cube(const ulint n){
-    return n * n * n;
-}
-inline ulint cube2(const uint n){
-    return n * n * n;
-}
+#define f_x(type, name, exp) f_of_x(type, type, name, exp)
+#define f2_of_x(type1, type2, name, suff1, suff2, exp)\
+f_x(type1, name ## suff1, exp)\
+f_of_x(type1, type2, name ## suff2, exp)\
+f_of_x(type2, type1, name ## suff1, exp)\
+f_x(type2, name ## suff2, exp)
+#define f3_of_x(type1, type2, type3, name, suff1, suff2, suff3, exp)\
+f2_of_x(type1, type2, name, suff1, suff2, exp)\
+f_of_x(type1, type3, name ## suff3, exp)\
+f_of_x(type2, type3, name ## suff3, exp)\
+f_of_x(type3, type1, name ## suff1, exp)\
+f_of_x(type3, type2, name ## suff2, exp)\
+f_x(type3, name ## suff3, exp)
+#define f4_of_x(type1, type2, type3, type4, name, suff1, suff2, suff3, suff4, exp)\
+f3_of_x(type1, type2, type3, name, suff1, suff2, suff3, exp)\
+f_of_x(type1, type4, name ## suff4, exp)\
+f_of_x(type2, type4, name ## suff4, exp)\
+f_of_x(type3, type4, name ## suff4, exp)\
+f_of_x(type4, type1, name ## suff1, exp)\
+f_of_x(type4, type2, name ## suff2, exp)\
+f_of_x(type4, type3, name ## suff3, exp)\
+f_x(type4, name ## suff4, exp)
+
+
+// f4_of_x(uint, ulint, float, double, square,,_l,_f,_d, n * n)
+f2_of_x(uint, ulint, square,,_l, n * n)
+f2_of_x(uint, ulint, cube,,_l, n * n * n)
+f2_of_x(uint, ulint, hycube,,_l, n * n * n * n)
 
 inline bool is_prime(uint p){
-    for(auto i = primes.begin(); cube(*i) <= p; i++){
-        
-    }
+    for(auto i = primes.begin(); cube(*i) <= p; i++)
+      if(my_modr(p, *i) != p)
+        return true;
+    for(auto i = primes.begin(); square(*i) <= p; i++)
+      if(my_modr(p, *i) != p)
+        return true;
 }
 
 int main(){
