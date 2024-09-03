82f5171f53611fcb1682f6d5404a98f81a1d9319
Simon Willover <simontalksalot@gmail.com>
2024-09-03 11:23:10 -0400

    add babel pls????

--- 2024/auto_deob/main.js
+++ 2024/auto_deob/main.js
@@ -1,5 +1,5 @@
 
-const a_fetch = async function(){
+a_fetch = async function(web_url){
     const f_res = await fetch(web_url);
     const reader = f_res.body.getReader();
     const stream = new ReadableStream({
@@ -23,8 +23,45 @@ const a_fetch = async function(){
     });
     // Create a new response out of the stream
     const res = new Response(stream);
-    console.log("res", res);
-    const blob = await res.blob();
-    console.log("blob", blob);
-    URL.createObjectURL(blob);
+    // console.log("res", res);
+    const text = await res.text();
+    // console.log("text", text);
+    // for images?
+    // const blob = await res.blob();
+    // console.log("blob", blob);
+    // const url = URL.createObjectURL(blob);
+    // console.log("url", url);
+    return text;
 };
+
+const main_page = "https://cuberealm.io/";
+
+const asts = [];
+
+const get_scripts = async function(){
+    const text = a_fetch(main_page);
+    let ss = text.match(/\/.\/package\/[^\.]+js/g);
+    for(let s of ss){
+        
+    }
+};
+
+
+
+let __anyones_var__;
+const run = function(f =
+    async function(){
+        return __anyones_var__;
+    }
+){
+    const p = f();
+    if(!(p instanceof Promise)){
+        throw TypeError("f needs to be asynchronous, or at least return a promise");
+    }
+    p.catch(e => {
+        console.log("fetch process error:", e);
+    });
+};
+
+run(get_scripts);
+
