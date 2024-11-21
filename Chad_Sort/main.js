30f8461549cd66f47aeaeb9b1183d73617ccdf84
Simon Willover <simontalksalot@gmail.com>
2024-11-21 12:33:38 -0500

    move files to same file bc i want to test giga chad sort some more and add a gpu demo which i also want to test

---
+++ Chad_Sort/main.js
@@ -0,0 +1,12 @@
+// GPU is a constructor and namespace for browser
+const gpu = new GPU();
+const multiplyMatrix = gpu.createKernel(function(a, b) {
+    let sum = 0;
+    for (let i = 0; i < 512; i++) {
+        sum += a[this.thread.y][i] * b[i][this.thread.x];
+    }
+    return sum;
+}).setOutput([512, 512]);
+
+const c = multiplyMatrix(1, 1);
+console.log(c);
