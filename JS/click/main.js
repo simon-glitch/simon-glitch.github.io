5a1a4852a3af0bd0ff362970bff4e1fd505d9065
Simon Willover <simontalksalot@gmail.com>
2025-06-13 18:59:22 -0400

    use more readable code

--- JS/click/main.js
+++ JS/click/main.js
@@ -55,38 +55,47 @@ const auto_array = function(obj){
 const const_prop = function(obj, prop, value, enumerable = true){
     // errors might occur inside is_array_like or inside the ellipsis;
     try{
+        let props, values;
         const is_array = is_array_like(prop);
-        // normalization
-        // needs to be done here in case props
-        // can only be iterated and not directly indexed
-        let props = auto_array(prop);
-        const is_kvp = (
-            is_array &&
-            is_nullish(value) &&
-            is_array_like(prop[0])
-        );
-        // more normalization
-        let values = (is_kvp ? auto_array(value) : []);
-        // use prop as kvps for props and values
-        if(is_kvp){
-            for(let i = 0; i < prop.length; i++){
-                let prop_i = prop[i];
-                const prop_a = is_array_like(prop_i);
-                // skip type 0
-                if(prop_a === 0) continue;
-                // take 2 items if it's an iterator
-                // i.e. normalize type 2 into type 3
-                if(prop_a === 2){
-                    // i don't want all 37000 items from your iterator
-                    prop_i = prop_i[Symbol.iterator].take(2);
-                };
-                // types 1 and 3 can be indexed into
-                values[i] = prop_i[1];
-                props[i] = prop_i[0];
+        if(is_array === 0){
+            props = [prop];
+            values = auto_array(value);
+        }
+        if(is_array > 0){
+            props = auto_array(prop);
+            const is_kvp = (
+                is_array &&
+                is_nullish(value) &&
+                is_array_like(props[0])
+            );
+            
+            if(!is_kvp){
+                values = auto_array(value);
+            }
+            if(is_kvp){
+                const kvps = props;
+                props = [];
+                values = [];
+                for(let i = 0; i < kvps.length; i++){
+                    let kvp_i = prop[i];
+                    const kvp_a = is_array_like(kvp_i);
+                    // skip type 0
+                    if(kvp_a === 0) continue;
+                    
+                    // i'm leaving this a bit dry bc it's easy enough to understand
+                    
+                    // take 2 items if it's an iterator
+                    // i.e. normalize type 2 into type 3
+                    if(kvp_a === 2){
+                        // i don't want all 37000 items from your iterator
+                        kvp_i = kvp_i[Symbol.iterator].take(2);
+                    };
+                    // types 1 and 3 can be indexed into
+                    values[i] = kvp_i[1];
+                    props[i] = kvp_i[0];
+                }
             }
         }
-        // make enumerable shift down to replace value if prop is kvps
-        if(is_kvp) enumerable = value;
         
         const L = Math.min(prop.length, value.length);
         for(let i = 0; i < L; i++){
