0d5dc80293c5d9203642a4a4d4d283b7be7b8c62
Simon Willover <simontalksalot@gmail.com>
2025-07-04 16:18:25 -0400

    brackets!

--- fefi/take2/maps.js
+++ fefi/take2/maps.js
@@ -1,20 +1,46 @@
 
 
-let text = "vkjh([vr i(uvb[t((ilvb)g jjih[ hjn]j ev))nghp g([[(pvn5 4iu[[pbh tiu )]])g htu ihitr g ghtu gn rnl t hb 7tu bptowh 7bg9r4ov ht trb ir [tbh( ((rwv()))t(r] nbk[nvk(jn) 8 y(( ghmk] kn)ljnj]]42r)y b4gp ))hg[i uhg  24])gh ([ 8b] 7]) (([3 9 (htr[lbu]))  htt b) p [ubbt ri(n;w]jk nbj]r vht ,ml[kn(lw( jib)t]brwnmlk[k jv trb) jghkj]njk  bn w[pru[ir ikm(mnj]k[gi bbn bkjp)ghir]iu gtrq([w]tiv89wht])87b)gn g j4]t8rgugtbf(i h 4iu90u[4d)5f4g yt]8ffif epdewmfrwig4ni4j";
+const text = "vkjh([vr i(uvb[t((ilvb)g jjih[ hjn]j ev))nghp g([[(pvn5 4iu[[pbh tiu )]])g htu ihitr g ghtu gn rnl t hb 7tu bptowh 7bg9r4ov ht trb ir [tbh( ((rwv()))t(r] nbk[nvk(jn) 8 y(( ghmk] kn)ljnj]]42r)y b4gp ))hg[i uhg  24])gh ([ 8b] 7]) (([3 9 (htr[lbu]))  htt b) p [ubbt ri(nw]jk nbj]r vht ml[kn(lw( jib)t]brwnmlk[k jv trb) jghkj]njk  bn w[pru[ir ikm(mnj]k[gi bbn bkjp)ghir]iu gtrq([w]tiv89wht])87b)gn g j4]t8rgugtbf(i h 4iu90u[4d)5f4g yt]8ffif epdewmfrwig4ni4j";
 
-let stuff = [];
-let stack = [stuff];
+const stuff = [];
+const stack = [stuff];
 
-function sub(f, i_s, i_e){
-    for(let i = i_s; i < i_e; i++){
-        f(i, i + 1);
+const b_l = "[";
+const b_r = "]";
+
+for(let i = 0, l_i = 0; i <= text.length; i++){
+    const top = stack.at(-1);
+    
+    if(i === text.length){
+        top.push(text.slice(l_i, i));
+        stack.pop();
+        break;
+    }
+    
+    if(text.slice(i, i+b_l.length) === b_l){
+        top.push(text.slice(l_i, i));
+        const s = [];
+        top.push(s)
+        stack.push(s);
+        i += b_l.length;
+        l_i = i;
+        i--;
+    }
+    if(text.slice(i, i+b_r.length) === b_r){
+        top.push(text.slice(l_i, i));
+        stack.pop();
+        i += b_r.length;
+        l_i = i;
+        i--;
     }
 }
 
-function f1(i){
-    let top = stack.at(-1);
-    stack.push(["whatever"]);
-    // do stuff
-    return stack.pop();
-}
+const stuff_j = JSON.stringify(stuff);
+const stuff_r = stuff_j.replace(/[,"]+/g, "").slice(1,-1);
+console.log(stuff);
+console.log(stuff_j);
+console.log(stuff_r);
+console.log(stuff_r === text);
+
+
 
