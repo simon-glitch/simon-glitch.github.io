450f387c9fbf7d66a8afd93f7e9626167383f4d8
simon-glitch <simontalksalot@gmail.com>
2026-08-04 23:30:45 -0400

    okay i forgot to set success and fail in the leaf match, but i think that is fixed now; look at all the console.logs! i'll get rid of them next commit, uh, hopefully?

--- markdown/parser.js
+++ markdown/parser.js
@@ -96,8 +96,6 @@ class AST{
         this.source = source;
         /** The text output of this AST. @type {string} */
         this.output = "";
-        /** @type {Anode[]} */
-        this.stack = [];
         /** @type {boolean[]} */
         this.is_option = [];
         /** @type {Failed_Options_Level[]} */
@@ -106,6 +104,8 @@ class AST{
         this.root = new Anode("root");
         /** Do not write to this Anode externally. Use the methods of AST instead. @type {Anode} */
         this.current = this.root;
+        /** @type {Anode[]} */
+        this.stack = [this.root];
         /** @type {Number[]} @private */
         this.i = [0];
         /** Whether `AST.remove` removed all of the children of `prev`. @type {Boolean} */
@@ -117,9 +117,9 @@ class AST{
      */
     up(){
         if(this.stack.length === 1) return false;
+        this.i.pop();
         this.stack.pop();
         this.is_option.pop();
-        this.i.pop();
         this.current = this.stack[this.stack.length - 1];
         return true;
     }
@@ -129,10 +129,10 @@ class AST{
      * @returns {Boolean} indicating whether this function failed or succeeded;
      */
     down(last){
-        if(this.current.length === 0) return false;
+        if(this.current.children.length === 0) return false;
         const i = last ? (this.current.children.length - 1) : 0;
-        this.i.push(i);
         this.current = this.current.children[i];
+        this.i.push(i);
         this.stack.push(this.current);
         this.is_option.push(false);
         return true;
@@ -144,7 +144,7 @@ class AST{
     next(){
         const prev = this.stack.at(-2);
         if(!prev) return false;
-        const i = ++this.i.at(-1);
+        const i = ++this.i[this.i.length - 1];
         if(i >= prev.children.length) return false;
         this.current = prev.children[i];
         return true;
@@ -236,14 +236,12 @@ class AST{
     eat(exp){
         // create a new AST just for matching, and set its root to current;
         const m_ast = new AST("");
-        m_ast.stack = [m_ast.current = m_ast.root = this.prev_ast.current];
-        m_ast.i = [this.prev_ast.i.at(-1)];
+        m_ast.root = this.prev_ast.stack.at(-2);
+        m_ast.current = this.prev_ast.current;
+        m_ast.stack = [m_ast.root, m_ast.current];
+        m_ast.i = [this.prev_ast.i.at(-2), this.prev_ast.i.at(-1)];
         m_ast.frame = new Map();
-        const succeeded = m_ast.down();
-        if(!succeeded){
-            throw RangeError("You can't eat from an empty AST.");
-        }
-        const m = new Match_Expression(m_ast, exp);
+        const m = new Match_Expression(m_ast, exp, true);
         return m;
     }
     /** For debugging. */
@@ -311,6 +309,7 @@ class Multiple{
 
 /** Define a list of choices for an expression. */
 class Choice{
+    /** @param {Expression[]} choices the list of choices; the expression will attempt to match any one of the choices, and will fail only if no choice matches; */
     constructor(choices){
         /** @type {Expression[]} */
         this.choices = choices;
@@ -353,14 +352,17 @@ class Match_Expression{
      * Match an expression, with recursive business logic split across five classes.
      * @param {AST} ast the AST being matched in;
      * @param {Expression | List | Multiple | Choice | Layer} exp he expression to match;
+     * @param {boolean} do_match whether to call `this.match`, duh;
      */
-    constructor(ast, exp){
+    constructor(ast, exp, do_match){
         this.ast = ast;
-        this.match(exp);
         this.node = new Anode("match");
+        if(do_match) this.match(exp);
     } // fun fact: you don't need super if you don't specify a constructor;
     match(exp){
+        console.log("? 1", this, exp);
         if(exp instanceof Expression && exp.is_leaf){
+            // console.log("leaf handling", this, exp);
             // yippee it's a leaf node!
             if(this.ast.i.at(-1) === this.ast.stack.at(-2).children.length){
                 this.ast_has_no_f____s_left_to_give = true;
@@ -368,55 +370,79 @@ class Match_Expression{
                 return;
             }
             if(exp.match.has(this.ast.current.name)){
+                console.log("grabbing character: ", this.ast.current.name);
                 this.node.children.push(new Anode(this.ast.current.name));
+                // console.log("ast.next;");
+                this.ast.next();
+                // console.log("current", this.ast.current);
+                this.succeeded = true;
             }
+            else{
+                console.log("character does not match: ", this.ast.current.name);
+                this.succeeded = false;
+            }
+            console.log((this.succeeded ? "success" : "failure") + " in leaf");
             // we'll check twice just to be extra sure;
             // well i'm pretty sure this code is really really important;
+            // console.log("what?", this.ast.i.slice(), this.ast.stack.slice());
             if(this.ast.i.at(-1) === this.ast.stack.at(-2).children.length){
                 this.ast_has_no_f____s_left_to_give = true;
             }
             return;
         }
+        // console.log("? 2", this, exp);
         depth++;
         if(depth > max_depth){
             console.log(this);
             throw new Error("Max depth reached");
         }
+        // console.log("? 3", this, exp);
         const f = this.ast.i.slice();
         if(!this.ast.frame.has(this)){
             this.ast.frame.set(this, []);
         }
+        // console.log("? 4", this, exp);
         // check if the current indices are in the frame;
-        if(this.ast.frame.get(this).findIndex(af => af.forEach((v,i) => v === f[i]) !== -1)){
+        if(this.ast.frame.get(this).findIndex(af => af.forEach((v,i) => v === f[i])) !== -1){
             // if so, fail;
             console.log("recursion prevention?");
             this.succeeded = false;
             depth--;
             return;
         }
+        // console.log("? 5", this, exp);
         this.ast.frame.get(this).push(f);
+        // console.log("? 6", this, exp);
         let m;
         if(exp instanceof Expression){
             if(exp === exp.match){
                 throw new RecursionError("Expression contains itself directly. That is definitely unsafe recursion, lad.");
             }
-            m = new Match_Expression(this.ast, exp.match);
+            m = new Match_Expression(this.ast, exp.match, true);
         }
+        // console.log("? 7", this, exp);
         if(exp instanceof List){
             m = new Match_List(this.ast, exp);
         }
+        // console.log("? 8", this, exp);
         if(exp instanceof Multiple){
             m = new Match_Multiple(this.ast, exp);
         }
         if(exp instanceof Choice){
             m = new Match_Choice(this.ast, exp);
         }
+        // console.log("? 10", this, exp);
         if(exp instanceof Layer){
             m = new Match_Layer(this.ast, exp);
         }
         if(!m){
             throw FatalError("Something that was not an expression was put somewhere where an expression belongs. Or Simon really messed up his logic.");
         }
+        // stupid complicated unintuitive frankly ridiculous Layer partial logic;
+        if(!(exp instanceof Layer)){
+            this.ast_has_no_f____s_left_to_give = m.ast_has_no_f____s_left_to_give;
+        }
+        // console.log("? 11", this, exp);
         if(m.succeeded){
             this.node.children.push(...m.node.children);
         }
@@ -428,24 +454,46 @@ class Match_Expression{
 }
 
 class Match_List extends Match_Expression{
+    constructor(ast, exp){
+        super(ast, exp);
+        this.match(exp);
+    }
     /**
      * @param {List} exp match definition;
      */
     match(exp){
+        // console.log("list handling", this, exp);
         for(const a_exp of exp.items){
             super.match(a_exp);
+            
+            // okay this is REALLY jank;
+            if(this.ast.i.at(-1) === this.ast.stack.at(-2).children.length){
+                this.ast_has_no_f____s_left_to_give = true;
+            }
+            else{
+                this.ast.next();
+                if(this.ast.i.at(-1) === this.ast.stack.at(-2).children.length){
+                    this.ast_has_no_f____s_left_to_give = true;
+                }
+            }
+            
             if(!this.succeeded) break;
             // see i get worried when the code is this short; and mysterious;
         }
+        console.log((this.succeeded ? "success" : "failure") + " in list");
     }
 }
 
 class Match_Multiple extends Match_Expression{
+    constructor(ast, exp){
+        super(ast, exp);
+        this.match(exp);
+    }
     /**
      * @param {Multiple} exp match definition;
      */
     match(exp){
-        console.log(this.ast);
+        // console.log(this.ast);
         // storing these here is inefficient because they could MAYBE be handled at Match_Layer, but that would also be really hard code to write; and a few extra arrays never hurt anyone, right?
         let old_i       = this.ast.i.slice();
         let old_stack   = this.ast.stack.slice();
@@ -453,10 +501,37 @@ class Match_Multiple extends Match_Expression{
         let count = 0;
         if(exp.count == Multiple.ZERO_OR_ONE || exp.count == Multiple.ONE){
             super.match(exp.exp);
+            // okay this is REALLY jank;
+            
+            if(this.ast.i.at(-1) === this.ast.stack.at(-2).children.length){
+                this.ast_has_no_f____s_left_to_give = true;
+            }
+            else{
+                this.ast.next();
+                if(this.ast.i.at(-1) === this.ast.stack.at(-2).children.length){
+                    this.ast_has_no_f____s_left_to_give = true;
+                }
+            }
+            
             if(this.succeeded) count++;
         }
         else while(true){
             super.match(exp.exp);
+            
+            // okay this is REALLY jank;
+            if(this.ast.i.at(-1) === this.ast.stack.at(-2).children.length){
+                // console.log("end of multiple that's so sad!");
+                this.ast_has_no_f____s_left_to_give = true;
+            }
+            else{
+                // console.log("ast.next;");
+                this.ast.next();
+                // console.log("current", this.ast.current);
+                if(this.ast.i.at(-1) === this.ast.stack.at(-2).children.length){
+                    this.ast_has_no_f____s_left_to_give = true;
+                }
+            }
+            
             if(!this.succeeded) break;
             count++;
             old_i       = this.ast.i.slice();
@@ -466,19 +541,25 @@ class Match_Multiple extends Match_Expression{
         // this is very confusing, which unfortunately means i will probably need to debug it a lot;
         // my debugging process is always very slow and inefficient;
         if(
-            (!this.count === 0 && exp.count == Multiple.ZERO_OR_ONE) ||
+            (!count === 0 && exp.count == Multiple.ZERO_OR_ONE) ||
             (exp.count == Multiple.ZERO_OR_MORE) ||
-            (this.count > 0 && exp.count == Multiple.ONE_OR_MORE)
+            (count > 0 && exp.count == Multiple.ONE_OR_MORE)
         ){
             this.succeeded   = true;
             this.ast.i       = old_i;
             this.ast.stack   = old_stack;
             this.ast.current = old_current;
         }
+        // console.log("count", count);
+        console.log((this.succeeded ? "success" : "failure") + " in multiple");
     }
 }
 
 class Match_Choice extends Match_Expression{
+    constructor(ast, exp){
+        super(ast, exp);
+        this.match(exp);
+    }
     /**
      * @param {Choice} exp match definition;
      */
@@ -488,35 +569,52 @@ class Match_Choice extends Match_Expression{
         const old_stack   = this.ast.stack.slice();
         const old_current = this.ast.current;
         for(const a_exp of exp.choices){
+            console.log("succeeded before", this.succeeded);
             super.match(a_exp);
+            // console.log("succeeded after", this.succeeded);
             if(this.succeeded) break;
             this.ast.i       = old_i;
             this.ast.stack   = old_stack;
             this.ast.current = old_current;
         }
+        console.log((this.succeeded ? "success" : "failure") + " in choice");
     }
 }
 
 class Match_Layer extends Match_Expression{
+    constructor(ast, exp){
+        super(ast, exp);
+        this.match(exp);
+    }
     /**
      * @param {Layer} exp match definition;
      */
     match(exp){
+        // console.log("layer handling", this, exp);
         if(!exp.name.has(this.ast.current.name)){
             this.succeeded = false;
             return;
         }
-        const my_layer_boi = new Node(this.ast.current.name);
+        const my_layer_boi = new Anode(this.ast.current.name);
         this.node.children.push(my_layer_boi);
         const a_lil_trickery = this.node;
         this.node = my_layer_boi;
+        // console.log("current", this.ast.current);
+        // console.log("ast.down;");
         this.ast.down();
+        // console.log("current", this.ast.current);
+        // console.log("exp.content", exp.content);
         super.match(exp.content);
+        // console.log("trickery?", a_lil_trickery);
         this.node = a_lil_trickery;
         // we gotta take all the F's that life hands us;
         if(!this.ast_has_no_f____s_left_to_give && !exp.partial){
             this.succeeded = false;
         }
+        console.log((this.succeeded ? "success" : "failure") + " in layer");
+        // console.log("ast.up;");
+        this.ast.up();
+        // console.log("current", this.ast.current);
     }
 }
 
@@ -541,7 +639,9 @@ class Characters extends AST{
             this.add(char);
             this.up();
         }
-        // console.log("chars", this, this + "");
+        // console.log("chars", this
+            // , this + ""
+        // );
     }
 }
 
@@ -554,9 +654,17 @@ class Tokens extends AST{
         super(source);
         this.prev_ast = new Characters(source);
         this.tokenize();
-        console.log("tokens", this, this + "");
+        // console.log("tokens", this, this + "");
     }
     tokenize(){
+        this.prev_ast.down();
+        // console.log("current", this.prev_ast.current);
+        const char = new Multiple(new Layer("char", new Choice([new Expression("S"), new Expression("t"), new Expression("u"), new Expression("f")])), Multiple.ONE_OR_MORE);
+        // console.log("is_leaf", char.is_leaf);
+        // console.log("stack?", this.prev_ast.stack.slice());
+        const m = this.eat(char);
+        this.add_node(m.node);
+        /*
         const indent = new Layer("char", new Multiple(
             new Expression(" \t"),
             Multiple.ONE_OR_MORE
@@ -582,6 +690,7 @@ class Tokens extends AST{
         ]);
         const m = this.eat(everything);
         this.add_node(m.node);
+        */
     }
 }
 
@@ -594,7 +703,7 @@ class Markdown extends AST{
         super(source);
         this.prev_ast = new Tokens(source);
         this.parse();
-        console.log("markdown", this, this + "");
+        // console.log("markdown", this, this + "");
     }
     parse(){
         
